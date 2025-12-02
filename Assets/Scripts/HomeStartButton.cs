ee0148f835b5092378d8524e6e05bfbe9193f4cd
Tien24205 <email_dang_ky_github_cua_Tien24205>
2025-12-02 23:52:47 +0700

    fix "Start" to move into Game Scene

--- Assets/Scripts/HomeStartButton.cs
+++ Assets/Scripts/HomeStartButton.cs
@@ -5,6 +5,6 @@ public class HomeStudyButton : MonoBehaviour
     public void OnStudyButtonPressed()
     {
         // Go to Game Scene using the Training GameState
-        GameStateMan.Instance.RequestState(GameStateMan.GameState.Training);
+        GameStateMan.Instance.RequestState(GameStateMan.GameState.GameScene);
     }
 }
